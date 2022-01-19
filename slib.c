06ff81f6802d7c160940ada163cf9b602ab353bf
Kader Arnold <mail@kader.systems>
2022-01-19 02:03:54 -0500

    added the rest of the socket functions, a write buffer function, port forward, etc.

--- slib.c
+++ slib.c
@@ -1,9 +1,10 @@
-#include <sys/stat.h>
+#include <unistd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <netinet/in.h>
@@ -12,11 +13,51 @@
 #include <signal.h>
 #include <strings.h>
 
+#define MAX 4096
 #define DIE(msg) perror(msg); exit(1);
+
+// declaration block //
+// declaration block //
 
 int broadcast_setup (char *);
 int open_listening_port(void);
+void accept_connection (int, char *);
+void port_forward (int, char *);
 int has_hole (void);
+int open_public_socket (char *);
+void com (int, int);
+
+// declaration block //
+// declaration block //
+
+void com (int to, int from) {
+
+  char buf[MAX];
+  int r, i, j;
+
+  r = read(to, buf, MAX);
+  while (r > 0) {
+    i = 0;
+    while (i < r) {
+      if ((j = write (from, buf+i, r-i)) == -1) {
+        DIE ("write");
+      }
+      i+=j;
+    }
+    r = read(to, buf, 4096);
+  }
+  if (r == -1) {
+    DIE("read");
+  }
+
+  shutdown (to, SHUT_RD);
+  shutdown (from, SHUT_WR);
+
+  close(to);
+  close(from);
+
+  exit(0);
+}
 
 int broadcast_setup (char * name) {
 
@@ -74,6 +115,78 @@ int open_listening_port() {
   }
 
   return socket_pid;
+}
+
+void port_forward (int client_sock, char * name) {
+
+  int pub_sock;
+  pid_t priv_pid;
+
+  pub_sock = open_public_socket (name);
+  if ((priv_pid = fork () == -1)) {
+    DIE("fork.");
+  }
+  if (priv_pid == 0)  {
+  // com (pub_sock, client_sock);
+  }
+  else  {
+  // com (client_sock, pub_sock);
+  }
+
+
+
+}
+
+int open_public_socket (char * name) {
+
+  int pub_sock;
+  struct hostent *pub;
+  struct sockaddr_in pub_addr;
+
+  if ((pub = gethostbyname(name)) == NULL) {
+    DIE("gethostbyname");
+  }
+
+  bzero ((char *) &pub_addr, sizeof(pub_addr));
+  pub_addr.sin_family = AF_INET;
+
+  bcopy ((char *)pub->h_addr,
+    (char *) &pub_addr.sin_addr.s_addr,
+    pub->h_length);
+
+  pub_addr.sin_port = htons(42170);
+  if ((pub_sock = socket(AF_INET,
+      SOCK_STREAM, 0) == -1)) {
+    DIE("socket");
+  }
+
+  if (connect(pub_sock, 
+    (struct sockaddr *) &pub_addr,
+    sizeof(pub_addr)) == -1) {
+      DIE("connect.");
+  }
+
+  return pub_sock;
+}
+
+void accept_connection (int spid, char * name) {
+
+  int local_sock;
+  pid_t pid;
+
+  if ((local_sock =
+      accept(spid, NULL, NULL))) {
+    DIE("accept");
+  }
+
+  if ((pid = fork()) == -1) {
+    DIE("fork.");
+  }
+
+  if (pid == 0) {
+    //forward_traffic ();
+  }
+
 }
 
 int has_hole () {
